c2d3a4fcdb15171f1494f2ddc2eafc47ec94df63
joeymc406 <>
2022-03-26 04:12:10 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// additon function
+let add = function(input1, input2){
+  let result = input1 + input2;
+	return result;
+}
+//subtraction function
+let subtract = function(input1, input2){
+  let result = input1 - input2;
+		return result;
+}
+// multiplication function
+let multiply = function(input1, input2){
+  let result = input1 * input2;
+		return result;
+}
+//divison function with conditional
+let divide = function(input1, input2){
+  if (divisor === 0) {
+    'not allowed.'
+  } else { let result = dividend / divisor;
+        return result;
+  }
+}
